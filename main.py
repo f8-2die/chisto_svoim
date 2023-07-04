385a071ca8ac594e327ddc24186ca1ab2c759ffc
jadesheart <jadesheartwork@gmail.com>
2023-07-04 16:23:48 +0400

    Улучшил структуру и создал файлы. Так же описал какие файлы за что отвечают

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+TOKEN = " "
 
 
-# Press the green button in the gutter to run the script.
+# эта функция будет ТОЧНО создавать самого bot и передавать в его в events
+def main():
+    bot = telebot.TeleBot(TOKEN)
+    # enevts = New() events(bot)
+    print("lorem ipsum")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
